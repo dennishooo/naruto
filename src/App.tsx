21b4dbebbb48b50e3ab3f661060907aa040f9800
dennishooo <hkhdennis@gmail.com>
2023-02-13 12:08:27 +0800

    test map fuunction

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
     async function fetchNaruto() {
@@ -21,7 +21,14 @@ function App() {
     <>
       <div>hello world</div>
 
-      <div>{JSON.stringify(data)}</div>
+      {data.map((character) => {
+        return (
+          <div>
+            <img src={character.images[0]} />
+            <div>{character.name}</div>
+          </div>
+        );
+      })}
     </>
   );
 }
